aec769a20ce9eb1c8ba2f6e21671f42a6c3d534c
Jenny Nguy <j.nguy809@gmail.com>
2019-09-30 04:13:29 +0000

    installed ejs

--- express_server.js
+++ express_server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com" 
+  "9sm5xK": "http://www.google.com"
 };
 
 app.get("/", (req, res) => {
