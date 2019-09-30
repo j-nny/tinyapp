eb5de1e4c5aa41d5b3f06e756ca7f6db6d486598
Jenny Nguy <j.nguy809@gmail.com>
2019-09-30 18:17:02 +0000

    added url shows

--- express_server.js
+++ express_server.js
@@ -26,6 +26,11 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars)
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
