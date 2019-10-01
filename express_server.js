6b99ec5344d8378ca4b5c315189ab0b2fdea5687
Jenny Nguy <j.nguy809@gmail.com>
2019-10-01 00:21:01 +0000

    added delete option to the /urls

--- express_server.js
+++ express_server.js
@@ -58,6 +58,11 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(urlDatabase[req.params.shortURL]);
 })
 
+app.post("/urls/:shortURL/delete", (req, res) =>{
+  delete urlDatabase[req.params.shortURL]
+  res.redirect("/urls")
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
