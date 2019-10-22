e4b82abe1a2c15d2dc014b986d48f21e9b6186bb
Carlos Garate <carlos.garate@rangle.io>
2019-10-21 20:17:19 -0400

    add helper functions

--- helpers.js
+++ helpers.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcrypt");
+
 //generates the short URL string of 6 chars
 let generateID = function() {
   let randomString = "";
@@ -30,8 +32,45 @@ let userURLs = function(user, urlDatabase) {
   return userDatabase;
 };
 
+const hashPassword = (password, saltRounds = 10) => {
+  let salt = bcrypt.genSaltSync(saltRounds);
+  return bcrypt.hashSync(password, salt);
+};
+
+const getEmailList = (stateUsers) => {
+  // returns an array with the VALUES (not KEYS) in stateUsers
+  const arrayOfUserObjects = Object.values(stateUsers);
+  // reduce is your best friend you can do pretty much everything with it
+  return arrayOfUserObjects.reduce(
+    (arrayToBeReturned, currentUserObject) => [...arrayToBeReturned, currentUserObject.email],
+    [],
+  );
+};
+
+const emailExists = (stateUsers, email) =>
+  // See if the email exists already in the users object
+  getEmailList(stateUsers).some((userEmail) => userEmail === email);
+
+const areValidEmailPassword = (stateUsers = undefined, email, password) => {
+  const arrayOfUserObjects = stateUsers ? Object.values(stateUsers) : [];
+  return (
+    arrayOfUserObjects.filter(
+      (userObject) =>
+        userObject.email === email &&
+        bcrypt.compareSync(password, userObject.password),
+    ).length > 0
+  );
+};
+
+const getArrayIndexOfUrl = (stateUsers, userId, urlId) =>
+  stateUsers[userId].shorturls.findIndex((url) => url === urlId);
+
 module.exports = {
+  emailExists,
   generateID,
+  getEmailList,
   getUserByEmail,
+  hashPassword,
   userURLs,
+  areValidEmailPassword,
 };
